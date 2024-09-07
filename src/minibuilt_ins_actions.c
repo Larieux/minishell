25db425dc5f2ad958cb5f518dbcdf86e3ec6a395
Larieux <130474297+Larieux@users.noreply.github.com>
2024-09-07 11:12:23 +0200

    echo fixed and ready for parsing

--- src/minibuilt_ins_actions.c
+++ src/minibuilt_ins_actions.c
@@ -6,7 +6,7 @@
 /*   By: jlarieux <jlarieux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 17:17:37 by jlarieux          #+#    #+#             */
-/*   Updated: 2024/09/06 19:16:34 by jlarieux         ###   ########.fr       */
+/*   Updated: 2024/09/07 11:07:02 by jlarieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,15 @@
 
 int	ft_echo(char *str, bool n)
 {
-	char	**dtmp;
-	char	*tmp;
 	char	*res;
 	char	*ptr;
-	int		i;
-	int		j;
 
-	i = 0;
-	j = 0;
-	tmp = ft_strdup(str);
-	ptr = ft_strchr(tmp, '\n');
-	while (ptr != NULL)
-	{
-		i++;
-		ptr++;
-		ptr = ft_strchr(ptr, '\n');
-	}
-	dtmp = ft_split(tmp, '\n');
-	free(tmp);
-	tmp = ft_strjoin(dtmp[j], "\\n");
-	free(dtmp[j]);
-	while (j < i)
-	{
-		dtmp[j] = ft_strjoin(tmp, dtmp[j + 1]);
-		free(tmp);
-		if (j < i - 1)
-		{
-			tmp = ft_strjoin(dtmp[j + 1], "\\n");
-			free(dtmp[j + 1]);
-		}
-		j++;
-	}
+	res = ft_strdup(str);
 	if (n == true)
-		res = ft_strdup(dtmp[j - 1]);
-	else
-		res = ft_strjoin(dtmp[j - 1], "\n");
-	free(dtmp);
+	{
+		ptr = ft_strrchr(res, '\n');
+		*ptr = '\0';
+	}
 	ft_putstr_fd(res, STDOUT_FILENO);
 	return (free(res), 0);
 }
--- src/minibuilt_ins_actions_utils.c
+++ src/minibuilt_ins_actions_utils.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   minibuilt_ins_actions_utils.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlarieux <jlarieux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/04 14:13:32 by jlarieux          #+#    #+#             */
-/*   Updated: 2024/09/07 11:10:10 by jlarieux         ###   ########.fr       */
+/*   Created: 2024/09/07 09:01:05 by jlarieux          #+#    #+#             */
+/*   Updated: 2024/09/07 10:08:54 by jlarieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	t_minist	s_minis;
-
-	s_minis.arg_c_or = argc;
-	s_minis.args = argv;
-	s_minis.env = envp;
-	pipex(&s_minis);
-	free(s_minis.infile); //to rm
-	free(s_minis.outfile); //to rm
-	return (0);
-}
